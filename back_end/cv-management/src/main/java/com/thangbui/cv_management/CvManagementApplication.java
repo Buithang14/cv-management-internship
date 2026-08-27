1a91d26331481a7aa37e8081436a990407f2eef3
BuiThang <buithag2482005@gmail.com>
2026-08-27 17:31:13 +0700

    Thêm mã nguồn back-end

---
+++ back_end/cv-management/src/main/java/com/thangbui/cv_management/CvManagementApplication.java
@@ -0,0 +1,13 @@
+package com.thangbui.cv_management;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CvManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CvManagementApplication.class, args);
+	}
+
+}
---
+++ back_end/cv-management/src/test/java/com/thangbui/cv_management/CvManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.thangbui.cv_management;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CvManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
